c6cf3a9e155644a61640cc0bf6bd24429ddfd4f8
Gabriel de Oliveira Silva <gabriel.oliveira.s177@gmail.com>
2023-03-14 20:35:23 -0400

    backup pra trocar de OS

---
+++ SistemaBanco/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace SistemaBanco;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        /* string nomePessoa;
+        float saldo = 1450.0f;
+        int valorSaque = 0;
+
+        Console.WriteLine("Digite seu nome: ");
+        nomePessoa = Console.ReadLine() ?? "";
+
+
+        Console.WriteLine("Digite o valor do saque");
+        valorSaque = int.Parse(Console.ReadLine());
+
+        bool verificacao = (saldo > valorSaque);
+        saldo = verificacao ? saldo - valorSaque : saldo;
+
+        Console.WriteLine($"Seu saldo {((verificacao) ? "" : "não")} foi aprovado");
+
+        Console.WriteLine($"Seu saldo é: {saldo}");
+        Console.ReadKey(); */
+
+        Console.WriteLine("Insira o primeiro Número");
+        int numero1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Insira o segundo Número");
+        int numero2 = int.Parse(Console.ReadLine());
+
+
+        Console.WriteLine($"A soma de {numero1} + {numero2} é: {numero1 + numero2}");
+
+    }
+}
---
+++ aula1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace aula1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
