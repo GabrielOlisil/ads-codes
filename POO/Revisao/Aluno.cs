e0e6aed28a34b1e34469bc68bf8c85a6167cd892
Gabriel de Oliveira Silva <gabriel.oliveira.s177@gmail.com>
2024-07-17 20:30:30 -0400

    Chama

---
+++ POO/Revisao/Aluno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1;
+
+class Aluno
+{
+    private DateTime _dataNasc;
+    private int _idade;
+
+    public Aluno(DateTime dataNasc)
+    {
+        _dataNasc = dataNasc;
+        if (dataNasc.Month > DateTime.Now.Month)
+        {
+            _idade = DateTime.Now.Year - dataNasc.Year - 1;
+            return;
+        }
+        if (dataNasc.Month == DateTime.Now.Month && dataNasc.Day >= DateTime.Now.Day)
+        {
+            _idade = DateTime.Now.Year - dataNasc.Year - 1;
+            return;
+        }
+        _idade = DateTime.Now.Year - dataNasc.Year;
+    }
+
+    public DateTime DataNasc { get => _dataNasc; }
+    public int Idade { get => _idade; }
+}
